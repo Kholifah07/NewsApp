e3acee0f4e2c1137e8ecc93de277095ca0fc4d87
Kholifah07 <siti.nurkholifah@students.utdi.ac.id>
2022-12-29 11:59:59 +0700

    1

---
+++ app/src/main/java/com/kadirkuruca/newsapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.kadirkuruca.newsapp.util
+
+const val API_KEY = "846e741f4c784ee998d598704dfea073"
+const val BASE_URL = "https://newsapi.org/"
+const val SEARCH_NEWS_TIME_DELAY = 500L
+const val QUERY_PAGE_SIZE = 20
